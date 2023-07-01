e3fe6b9e40ab2d8efe8377687a9a6854d565adb6
guy32807 <guy32807@comcast.net>
2023-06-30 22:17:57 -0400

    Implemented the translation feature

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,13 @@
+from translator import englishToFrench, frenchToEnglish
+import unittest
+
+class Test(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+        self.assertEqual(englishToFrench('car'), 'voiture')
+        self.assertEqual(englishToFrench('I love my wife'), "J'aime ma femme.")
+        self.assertEqual(frenchToEnglish('maison'), 'mosaic to you')
+        self.assertEqual(frenchToEnglish('Comment allez vous?'), 'How are you doing? ')
+        
+unittest.main()
